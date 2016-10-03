dfd281172458b0c84ca369c3d49f85043dc7b050
andrew-a-gusev <andrew.a.gusev@gmail.com>
2016-10-03 15:17:10 +0300

    CodeReview #1

--- Calculator/src/main/java/Operands.java
+++ Calculator/src/main/java/Operands.java
@@ -1,4 +1,7 @@
-
+/**
+ * This is a concrete class, while in real life, operands can have a single value (e.g. sqrt(2))
+ * What can be done to fix it ?
+ */
 public class Operands {
     public Integer var1;
     public Integer var2;
